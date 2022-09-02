b88db279737e8befc3ec88e8a99b3907df9642fc
Jorge <91227064+JRSalica@users.noreply.github.com>
2022-09-02 15:13:51 -0300

    order status added

--- src/models/Order.js
+++ src/models/Order.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// const Product = require('./Product');
+const VALID_STATUS = [
+  'PENDING',
+  'PLACED',
+  'CANCELLED',
+];
 
 const OrderSchema = new Schema({
   products: [
@@ -29,25 +33,13 @@ const OrderSchema = new Schema({
   },
   total: {
     type: Number,
-  }
+  },
+  status: {
+    type: String,
+    enum: VALID_STATUS,
+    default: VALID_STATUS[0],
+  },
 }, { timestamps: true });
-
-// OrderSchema.pre('save', async function(next){
-//   try {
-//     this.amount = 0;
-//     this.products.forEach(async (product) => {
-//       product.totalPrice = 0;
-//       productInfo = await Product.findById(product.productId);
-//       console.log(productInfo.price);
-//       product.totalPrice = product.quantity * productInfo.price;
-//       console.log(product.totalPrice);
-//       this.amount = this.amount + product.totalPrice;
-//     });
-//     next();
-//   } catch (error) {
-//     throw console.error('Error al calcular montos.', error);
-//   }
-// });
 
 OrderSchema.set('toJSON', { transform: (document, returnedObject) =>{
   delete returnedObject.__v;
